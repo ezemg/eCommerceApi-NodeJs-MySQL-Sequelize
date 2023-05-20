236c0985183ee35530d10aacfd900b3ea34fbb60
ezemg <ezequielmatiasgr@gmail.com>
2023-05-20 19:06:24 -0300

    Primer commit

---
+++ controllers/usuariosController.js
@@ -0,0 +1,45 @@
+const { response } = require('express');
+
+const usuariosGet = (req, res = response) => {
+  const { user = 'Undefined User', nombre = 'Undefined Name' } = req.query;
+  res.json({
+    msg: 'get API - controlador',
+    user,
+    nombre,
+  });
+};
+
+const usuariosPut = (req, res = response) => {
+  const id = req.params.id;
+
+  res.status(500).json({
+    msg: 'put API - controlador',
+    id,
+  });
+};
+const usuariosPost = (req, res = response) => {
+  const { nombre, edad } = req.body;
+  res.status(201).json({
+    msg: 'post API - controlador',
+    nombre,
+    edad,
+  });
+};
+const usuariosPatch = (req, res = response) => {
+  res.status(201).json({
+    msg: 'post PATCH - controlador',
+  });
+};
+const usuariosDelete = (req, res = response) => {
+  res.json({
+    msg: 'delete API - controlador',
+  });
+};
+
+module.exports = {
+  usuariosGet,
+  usuariosPut,
+  usuariosPost,
+  usuariosPatch,
+  usuariosDelete,
+};
